d709a450c559cfec6c3268aa54e0ef18b17b0538
guliyara <guliyara>
2020-06-11 21:50:44 -0400

    second code push

--- src/gitTest1/TestGitPush1.java
+++ src/gitTest1/TestGitPush1.java
@@ -3,8 +3,13 @@ package gitTest1;
 public class TestGitPush1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Syntax Technologies");
 
+		
+		
+		
+		
+		
 	}
 
 }
